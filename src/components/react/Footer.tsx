b7c1af68aac8a974f17a150e7b19312e46d553f3
Nicolas <alvaradonicolas317@email.com>
2025-05-09 17:34:49 -0300

    Refactor Footer component: fix typos, improve layout consistency, and enhance responsiveness for mobile devices

--- src/components/react/Footer.tsx
+++ src/components/react/Footer.tsx
@@ -5,14 +5,14 @@ const Footer = () => {
   return (
     <section className='flex flex-col p-10 max-sm:p-5 lg:p-20 lg:pb-10  bg-gradient-to-r from-black via-gray-950 to-black '>
       <div className='max-w-[1700px] mx-auto w-full'>
-      <div className='lg:flex justify-between itetms-center '>
-        <h2 className='text-5xl max-sm:text-4xl font-bold pb-5 md:w-[450px]'>¿Listo para transformar tu negocio?</h2>
-        <div className='lg:flex flex-col justify-center'>
-          <p className='font-medium pb-5 md:w-[450px] '>Estamos aquí para ayudarte a escalar, automatizar y lograr resultados reales. ¡Conversemos!</p>
+      <div className='lg:flex justify-between items-center   '>
+        <h2 className='text-5xl max-sm:text-4xl font-bold py-5 md:w-[450px]  max-sm:text-center'>¿Listo para transformar tu negocio?</h2>
+        <div className='lg:flex flex-col justify-center '>
+          <p className='font-medium pb-5 md:w-[450px] max-sm:text-center'>Estamos aquí para ayudarte a escalar, automatizar y lograr resultados reales. ¡Conversemos!</p>
           <div className='pb-20 lg:pb-10 max-sm:pb-8'>
           <a
 						href="#get-started"
-						className="group relative inline-flex items-center text-center px-6 py-3 overflow-hidden rounded-full text-white text-lg max-sm:text-base font-medium"
+						className="group relative inline-flex items-center text-center px-6 py-3 overflow-hidden rounded-full text-white text-lg max-sm:text-base font-medium max-sm:flex max-sm:items-center max-sm:justify-center max-sm:w-10/12 max-sm:mx-auto"
 					>
 						{/* Button background layers */}
 						<span className="absolute inset-0 bg-gradient-to-r from-blue-600 via-purple-600 to-blue-600 animate-gradient-x bg-size-200"></span>
@@ -32,14 +32,14 @@ const Footer = () => {
 						</span>
 					</a>
           </div>
-          <ul className='flex justify-between text-white/40 max-sm:flex-col gap-3 mb-4  '>
+          <ul className='flex justify-between text-white/40 max-sm:flex-col gap-3 mb-4   '>
             <li className='hover:text-white transition-colors cursor-pointer'>Privacy Policy</li>
             <li className='hover:text-white transition-colors cursor-pointer'>Terms</li>
             <li className='text-white cursor-pointer'>suppor@tallgiardini.com</li>
           </ul>
         </div>
       </div>
-      <ul className='flex gap-10 pb-12 max-sm:pb-3'>
+      <ul className='flex gap-10 pb-12 max-sm:pb-3 '>
         <a className='transition-transform duration-200 hover:scale-125' href=''><li><FaFacebookF className='size-5' /></li></a>
         <a className='transition-transform duration-200 hover:scale-125' href=''><li><FaLinkedinIn className='size-5' /></li></a>
         <a className='transition-transform duration-200 hover:scale-125' href=''><li><FaInstagram className='size-5' /></li></a>
